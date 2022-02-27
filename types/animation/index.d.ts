fa6085b55ba5d7d70f02eadaf9b6f0f7e72ffedb
Emeka the Developer <showgame360@gmail.com>
2022-02-27 01:10:08 -0800

    #v2-alpha.0

--- types/animation/index.d.ts
+++ types/animation/index.d.ts
@@ -1,1 +1,9 @@
+import type { Animate } from "../../../dative";
 
+export declare function flip({ animate, duration, delay }: Animate): void;
+
+export declare function fadeOut({ animate, duration, delay }: Animate): void;
+
+export declare function bubble({ animate, duration, delay }: Animate): void;
+
+export declare function swing({ animate, duration, delay }: Animate): void;
